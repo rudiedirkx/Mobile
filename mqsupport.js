b20fd9ddb4801e206b1a120a0da2223beca41a9e
Rudie Dirkx <github@hotblocks.nl>
2011-05-11 23:14:05 +0200

    much improved mqsupport script

--- mqsupport.js
+++ mqsupport.js
@@ -1,99 +1,77 @@
 
 (function() {
 
+	/** config **/
+	var speed = 500,
+		orientation = false;
+	var scripts = document.getElementsByTagName('script'),
+		config = (scripts[scripts.length-1].getAttribute('src') || '').split('#'),
+		config = '&' + (config[1] || '') + '&';
+	if ( config ) {
+		if ( /&once&/.test(config) ) {
+			speed = 0;
+		}
+		else {
+			if ( (m = config.match(/&speed=(\d+)&/)) ) {
+				speed = parseInt(m[1]);
+			}
+		}
+		if ( config && 0 <= config.indexOf('orientation') ) {
+			orientation = true;
+		}
+	}
+//console.log('orientation: ' + orientation + "\nspeed: " + speed);
+
+	/** support functions **/
 	function each(list, fn) {
 		for ( var k=0; k<list.length; k++ ) {
 			fn(list[k], k, list);
 		}
 		return list;
 	}
-	function trim(str) {
-		return str.replace(/^\s+/, '').replace(/\s+$/, '');
-	}
-	function vpwidth() {
-		return document.documentElement.clientWidth;
-	}
-	function vpheight() {
-		return document.documentElement.clientHeight;
-	}
-	function filter(list) {
-		var r = [];
-		each(list, function(v) {
-			if ( v ) r.push(v);
-		});
-		return r;
-	}
-	function getHandler(name) {
-		var handlers = {
-			'orientation': function(val) {
-				if ( 'portrait' == val && vpwidth() < vpheight() ) {
-					return true;
+
+	/** test stylesheets **/
+	function evalLinkScripts(sss) {
+		if ( orientation ) {
+			var html = document.documentElement, no = document.documentElement.clientWidth > document.documentElement.clientHeight ? 'landscape' : 'portrait';
+			html.className = html.className.replace(/(^|\s)(landscape|portrait)(\s|$)/g, '$1$3').replace(/\s+$/, '') + ' ' + no;
+		}
+		each(sss, function(ss) {
+			if ( ss.mqsupport ) {
+				var enabled = ss.mqsupport();
+				if ( enabled === ss.disabled ) {
+					ss.disabled = !enabled;
 				}
-			},
-			'max-width': function(val) {
-				return vpwidth() <= parseFloat(val);
-			},
-			'min-width': function(val) {
-				return vpwidth() >= parseFloat(val);
-			},
-			'max-height': function(val) {
-				return vpheight() <= parseFloat(val);
-			},
-			'min-height': function(val) {
-				return vpheight() >= parseFloat(val);
 			}
-		};
-		return handlers[name];
-	}
-	function map(list, fn) {
-		var r = [];
-		each(list, function(el) {
-			r.push(fn(el));
 		});
-		return r;
 	}
-	function parseMediaAttribute(media) {
-		media = media.replace(/(all|handheld|only|print|projection|screen|device\-)/gi, '');
-		var ors = filter(map(media.split(','), trim));
-		var enable = true;
-		each(ors, function(conditions) {
-			var ands = filter(map(conditions.split('and'), trim)), musthave = ands.length, dohave = 0;
-			each(ands, function(condition) {
-				var x = condition.substr(1, condition.length-2).split(':');
-				var h = getHandler(x[0]);
-				if ( h && h(x[1]) ) dohave++;
-			});
-			if ( musthave && dohave < musthave ) enable = false;
-		});
-		return enable;
-	}
-	function mediaAttributeDoable(media) {
-		return '' != trim(media.replace(/(all|handheld|only|print|projection|screen|and|,)/gi, ''));
-	}
-	function evalLinkScripts() {
-		each(document.getElementsByTagName('link'), function(ss) {
-			var mq = ss.getAttribute('xmedia');
-			if ( mq ) {
-				ss.disabled = !parseMediaAttribute(mq);
-			}
-			else {
-				var mq = ss.getAttribute('media');
-				var doable = mq && mediaAttributeDoable(mq);
-				if ( doable ) {
+	/** initiate tests **/
+	function initLinkScripts() {
+		var sss = each(document.getElementsByTagName('link'), function(ss) {
+			var m = ss.getAttribute('media');
+			if ( m && m.match(/^only \(/) ) {
+				var media, fn = m.substr(5);
+				fn = fn.replace(/\((min|max)\-width:\s*(\d+)(?:px|)\)/g, function(match, minmax, width){
+					return '(document.documentElement.clientWidth ' + ( 'min' == minmax ? '>' : '<' ) + '= ' + width + ')';
+				}).replace(/\(orientation:(portrait|landscape)\)/g, function(match, orientation) {
+					return '(document.documentElement.clientWidth ' + ( 'landscape' == orientation ? '>' : '<' ) + ' document.documentElement.clientHeight)';
+				}).replace(/\s+and\s+/g, ' && ');
+				eval('fn = function(){ return ' + fn + '; }');
+				try {
+					var enabled = fn();
+					ss.disabled = !enabled;
 					ss.removeAttribute('media');
-					ss.setAttribute('xmedia', mq);
-					ss.disabled = !parseMediaAttribute(mq);
+					ss.mqsupport = fn;
+				}
+				catch (ex) {
+					// 'invalid' media query, so do nothing, so let the browser handle this
 				}
 			}
 		});
+		if ( !isNaN(speed) && 0 < speed ) {
+			setInterval((function(ss) { return function() { evalLinkScripts(ss); }; })(sss), speed);
+		}
 	}
-	evalLinkScripts();
-	var ua = navigator.userAgent.toLowerCase();
-	function contains(ab, a) {
-		return -1 != ab.indexOf(a);
-	}
-	if ( !contains(ua, 'mobile') && !contains(ua, 'opera mini') ) {
-		setInterval(evalLinkScripts, 900);
-	}
+	initLinkScripts();
 
 })();
